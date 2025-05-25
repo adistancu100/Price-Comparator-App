a8aeb2b56e13f4846b5cfe69cd4920d5e9ee3f98
Adi <adrian-daniel.stancu@s.unibuc.ro>
2025-05-25 20:40:30 +0300

    add Product model

--- src/main/java/com/adrian/price_comparator/Model/Product.java
+++ src/main/java/com/adrian/price_comparator/Model/Product.java
@@ -1,4 +1,88 @@
 package com.adrian.price_comparator.Model;
 
+import java.time.LocalDate;
+
 public class Product {
+
+    private String productId;
+    private String name;
+    private String category;
+    private String brand;
+    private double quantity;
+    private String unit;
+    private double price;
+    private String currency;
+    private LocalDate date;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
