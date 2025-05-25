79253fb09a2279e2cea33a4a3bef90134b0ef0d8
Adi <adrian-daniel.stancu@s.unibuc.ro>
2025-05-25 22:20:23 +0300

    added csv files for testing

--- src/main/java/com/adrian/price_comparator/PriceComparatorApplication.java
+++ src/main/java/com/adrian/price_comparator/PriceComparatorApplication.java
@@ -1,8 +1,12 @@
 package com.adrian.price_comparator;
 
+import com.adrian.price_comparator.model.Discount;
+import com.adrian.price_comparator.util.DiscountCSVUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.util.List;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class
@@ -11,6 +15,10 @@ public class PriceComparatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriceComparatorApplication.class, args);
+
+		String filePath = "src/main/resources/csv/kaufland_discounts_2025-05-01.csv";
+		List<Discount> discounts = DiscountCSVUtils.readDiscountCSV(filePath);
+		discounts.forEach(System.out::println);
 	}
 
 }
